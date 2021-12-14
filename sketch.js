0203093cc9242dc08761e16b72963e565fd02311
Matheus Cabral Calil <93673317+XaulinMatadorDeGaivota@users.noreply.github.com>
2021-12-14 14:50:41 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,11 +1,11 @@
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
-const Body = Matter.Body;
+//const Body = Matter.Body;
 
-let engine;
-let world;
-var ground;
+var engine;
+var world;
+
 
 var rock,ball,qua;
 
@@ -14,13 +14,14 @@ function preload(){
 }
 
 function setup() {
-	createCanvas(800, 700);
+	createCanvas(550, 600);
 	engine = Engine.create();
 	world = engine.world;
 
-	var Edge_options={
-		isStatic: true
-	  };
+	//ARIANA: VOCÊ CRIOU SSO E NÃO USOU 
+	//var Edge_options={
+		//isStatic: true
+	 // };
 
 	  var qua_options = {
 		restitution:0.05,
@@ -28,39 +29,42 @@ function setup() {
 		frictionAir:0  
 	  }
 
-	  var ground_options ={
+	  var plane_options ={
 		isStatic: true
-	  };
+	  }
 
-	 rock_options={
+	  plane = Bodies.rectangle(600,580,1200,2,plane_options);
+	  World.add(world,plane);
+
+	 var rock_options={
 		restitution:0.85,
 		fiction:0.01,
 		frictionAir:0.01
-	  }
+	  };
 	
-	 ball_options = {
+	 var ball_options = {
 		restitution: 0.95,
 		friction:1,
 		frictionAir:0.01
-	  }
+	  };
 	   
-	Engine.run(engine);
+	//Engine.run(engine);
   
-	ground = Bodies.rectangle(200,700,400,20,ground_options);
-	World.add(world,ground);
+	
 
 	ball = Bodies.circle(100,10,20,ball_options);
 	World.add(world,ball);
 	
-	Edge=Bodies.rectangle(100,700,100,20,Edge_options);
-  	World.add(world,Edge);
+	//Edge=Bodies.rectangle(100,700,100,20,Edge_options);
+  	//World.add(world,Edge);
 
-	rock=Bodies.circle(300,20,10,rock_options);
+	rock=Bodies.rectangle(300,20,10,10,rock_options);
   	World.add(world,rock);
-
-	qua = Bodies.rectangle(100,10,20,qua_options);
+	//ARIANA: AQUI VOCÊ TINHA DITO QUE ERA CIRCULAR PORÉM NO FUNCTIONDRAW VOCÊ ESCREVEU QUADRADO USANDO RECT
+	qua = Bodies.rectangle(100,10,20,10,qua_options);
 	World.add(world,qua);
 
+	
 	rectMode(CENTER);
   	ellipseMode(RADIUS);
 }
@@ -70,13 +74,15 @@ function draw() {
   background("gray");	
   Engine.update(engine);
 
+  rect(plane.position.x,plane.position.y,1200);
+
   ellipse(ball.position.x,ball.position.y,30);
   //rect(ball.position.x,ball.position.y,400,20);
 
-  rect(rock.position.x,rock.position.y,40);
+  rect(rock.position.x,rock.position.y,40,40);
 
-  rect(qua.position.x,qua.position.y,50);
+  rect(qua.position.x,qua.position.y,50,40);
 
-  drawSprites();
+  //drawSprites();
  
 }
